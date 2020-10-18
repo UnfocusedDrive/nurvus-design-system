efa86d9f17e4644a2ed1f5e9977358313c9cefe5
PowerGlove <john.yagiz@gmail.com>
2020-10-17 19:46:10 -0700

    Basic Webpack Install (v5)

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  devServer: {
+    contentBase: './dist',
+  },
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
