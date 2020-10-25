f53d7ac776fae2f428944468288186cb990e2513
PowerGlove <john.yagiz@gmail.com>
2020-10-24 18:43:00 -0700

    Install SCSS

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import './Button.scss';
 
 export default class Button extends React.Component {
   render() {
+    const { children } = this.props;
+
     return (
-      <button>I am button</button>
+      <button className="nurvus button">
+        { children }
+      </button>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from './components/Button.jsx';
+import './index.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+      asdsadaszzz
+      <Button>
+        Button 1
+      </Button>
+      <Button>
+        Button 2
+      </Button>
+    </div>
+    );
+  }
+}
 
 ReactDOM.render(
-  <div>
-    asdsadas
-    <Button />
-  </div>,
+  <App />,
   document.body
 );
