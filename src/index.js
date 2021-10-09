cf7b1cd5f05067c45a84bc6aaaf551339e121c54
John Yagiz <john.yagiz@gmail.com>
2021-10-09 09:04:54 -0700

    Corrected Exports

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-export { default as Button } from './components/Button';
-export { default as Progress } from './components/Progress';
-export { default as StackLayout } from './components/StackLayout';
+export { default as Button } from './components/Button.jsx';
+export { default as Progress } from './components/Progress/Progress.jsx';
+export { default as StackLayout } from './components/StackLayout/StackLayout.jsx';
