ed6ac82753f00fd3cc21409f388d259ac0ac5424
John Yagiz <john.yagiz@gmail.com>
2021-10-24 08:48:00 -0700

    Text: Example for Overflow

--- src/components/Text/Text.jsx
+++ src/components/Text/Text.jsx
@@ -1,30 +1,6 @@
 import React from 'react';
 import classNames  from 'classnames';
 import CONSTANT from '../../constants.js';
-// import './Text.scss';
-
-// function getTag(level) {
-//   switch (level) {
-//     case 1:
-//       return 'h1';
-//       break;
-//     case 2:
-//       return 'h2';
-//       break;
-//     case 3:
-//       return 'h3';
-//       break;
-//     case 4:
-//       return 'h4';
-//       break;
-//     case 5:
-//       return 'h5';
-//       break;
-//     case 6:
-//     default:
-//       return 'h6';
-//  }
-// }
 
 /**
  * Text component.
@@ -34,7 +10,6 @@ export default function Text(props) {
     const {
       children,
       className,
-      level = 1,
       ...restProps
     } = props;
 
@@ -46,13 +21,4 @@ export default function Text(props) {
         { children }
       </span>
     );
-
-    // return React.createElement(
-    //   getTag(level),
-    //   {
-    //     className: classNames(`${CONSTANT.CLASS_PREFIX}`, 'Text', className),
-    //     ...restProps
-    //   },
-    //   children
-    // );
 }
