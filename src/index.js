17aad85cab3ac4c081aae75d500c3c793c7b1cf6
John Yagiz <john.yagiz@gmail.com>
2021-10-25 02:11:39 -0700

    Last call

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 // Components
-export Button from './components/Button/Button.jsx';
-export ButtonGroup from './components/ButtonGroup/ButtonGroup.jsx';
-export Progress from './components/Progress/Progress.jsx';
-export Layout from './components/Layout/Layout.jsx';
-export Menu from './components/Menu/Menu.jsx';
-export Text from './components/Text/Text.jsx';
-export Title from './components/Title/Title.jsx';
+import Button from './components/Button/Button.jsx';
+import ButtonGroup from './components/ButtonGroup/ButtonGroup.jsx';
+import Progress from './components/Progress/Progress.jsx';
+import Layout from './components/Layout/Layout.jsx';
+import Menu from './components/Menu/Menu.jsx';
+import Text from './components/Text/Text.jsx';
+import Title from './components/Title/Title.jsx';
+
+export {
+  Button,
+  ButtonGroup,
+  Progress,
+  Layout,
+  Menu,
+  Text,
+  Title
+}
