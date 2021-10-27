c6860dd3bb36291b242bb22454d35065b7af9fff
John Yagiz <john.yagiz@gmail.com>
2021-10-27 07:37:45 -0700

    Install prop-types

--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import classNames  from 'classnames';
 import CONSTANT from '../../constants.js';
 import './Link.scss';
@@ -7,19 +8,32 @@ import './Link.scss';
  * Link component.
  * @returns {ReactElement} of Link component.
  */
-export default function Link(props) {
-    const {
-      children,
-      className,
-      ...restProps
-    } = props;
+function Link(props) {
+  const {
+    children,
+    className,
+    ...restProps
+  } = props;
 
-    return (
-      <a
-        className={ classNames(`${CONSTANT.CLASS_PREFIX}`, 'link', className) }
-        { ...restProps }
-      >
-        { children }
-      </a>
-    );
+  return (
+    <a
+      className={ classNames(`${CONSTANT.CLASS_PREFIX}`, 'link', className) }
+      { ...restProps }
+    >
+      { children }
+    </a>
+  );
 }
+
+Link.propTypes = {
+  /**
+   * Custom class name.
+   */
+  className: PropTypes.string,
+  /**
+   * Content to render.
+   */
+  children: PropTypes.node
+};
+
+export default Link;
