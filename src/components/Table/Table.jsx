842c53e4f546a8d9a893ca87b7b99c8ff321071f
John Yagiz <john.yagiz@gmail.com>
2022-04-24 11:59:00 -0700

    Table

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,32 +1,86 @@
 import React from 'react';
-export default class Table extends React.Component {
+import PropTypes from 'prop-types';
+import classNames  from 'classnames';
+import CONSTANT from '../../constants.js';
+import './Table.scss';
 
+class Table extends React.Component {
+  renderCells(rowProps) {
+    const { columns } = this.props;
 
-  getRows() {
-    const{ data } = this.props;
-
-
-    return data.map(row => {
+    return columns.map((columnProps, i) => {
+      const { dataKey } = columnProps;
 
       return (
-        <tr>
-          <td>
-            { row.id }
-          </td>
-          </tr>
+        <td key={ dataKey || i }>
+          { rowProps[columnProps.dataKey] }
+        </td>
       );
     });
+  }
 
+  renderHeader() {
+    const { columns } = this.props;
+
+    return columns.map((columnProps, i) => {
+      const { dataKey, title } = columnProps;
+
+      return (
+        <th key={ dataKey || i }>
+          { title }
+        </th>
+      );
+    });
+  }
+
+  renderRows() {
+    const{ data } = this.props;
+
+    return data.map((rowProps, i) => {
+      const { key } = rowProps;
+
+      const isEven = i % 2 == 0;
+
+      return (
+        <tr key={ key || i } data-even={ isEven }>
+          { this.renderCells(rowProps) }
+        </tr>
+      );
+    });
   }
 
   render() {
+    const { className } = this.props;
     return (
-      <table>
+      <table
+        className={ classNames(`${CONSTANT.CLASS_PREFIX}`, 'table', className) }
+      >
+        <thead>
+          <tr>
+            { this.renderHeader() }
+          </tr>
+        </thead>
         <tbody>
-        { this.getRows() }
-
-          </tbody>
+          { this.renderRows() }
+        </tbody>
       </table>
     );
   }
 }
+
+Table.propTypes = {
+  /**
+   * Custom class name.
+   */
+  className: PropTypes.string,
+  /**
+   * Columns to render in table.
+   */
+  columns: PropTypes.array,
+  /**
+   * Data to render in table.
+   */
+  data: PropTypes.array
+ };
+
+export default Table;
