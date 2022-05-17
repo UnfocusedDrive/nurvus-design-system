020fbc73be77c275279cd5f4a393b51b52cf6709
John Yagiz <john.yagiz@gmail.com>
2022-05-16 19:28:53 -0700

    Progress Component

--- src/components/Progress/Progress.jsx
+++ src/components/Progress/Progress.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import classNames  from 'classnames';
+import CONSTANT from '../../constants.js';
 import './Progress.scss';
 
-export default class Progress extends React.Component {
+/**
+ * Progress component. Use it to track progress!
+ * @returns {ReactElement} of Progress component.
+ */
+class Progress extends React.Component {
   render() {
-    const { children } = this.props;
+    const { className, percent = 0 } = this.props;
 
     return (
-      <div className="nurvus progress" />
+      <div className={ classNames(`${CONSTANT.CLASS_PREFIX}`, 'progress', className) }>
+        <div className="bar" style={{ width: `${percent}%` }} />
+      </div>
     );
   }
 }
+
+Progress.propTypes = {
+  /**
+   * Custom class name.
+   */
+   className: PropTypes.string,
+   /**
+    * Progress percent.
+    */
+    percent: PropTypes.number
+  };
+
+export default  Progress;
--- src/components/Progress/Progress.md
+++ src/components/Progress/Progress.md
@@ -2,6 +2,7 @@
 import Layout from '../Layout/Layout.jsx';
 <Layout>
   <Progress />
+  <Progress percent={ 50 } />
   <Progress percent={ 100 } />
 </Layout>
 ```
